872abca63c202837c89f1295c1c14cf7b5a1bd64
Sean-MainDesktop <grubbsean19@gmail.com>
2019-03-18 08:32:32 -0400

    Added UI functions
    added questions to decide if you want to decode or encode
    
    Also fixed the encode to do the entire line other than doing just the next string

--- FileEncoding/Source.cpp
+++ FileEncoding/Source.cpp
@@ -42,20 +42,87 @@ int main()
 
 	//Asking for user input to create specific file name 
 
-	cout << "what would you like the file name to be?(Can include File Extenstion, Default is basic FILE)\n";
-	cin >> Filename;
+	//cout << "what would you like the file name to be?(Can include File Extenstion, Default is basic FILE)\n";
+	//getline(cin, Filename);
 
-	outputFile.open(Filename, fstream::in | fstream::out | fstream::trunc);
+	//outputFile.open(Filename, fstream::in | fstream::out | fstream::trunc);
 
 	//Asking user what value they would like to be encoded
 
-	cout << "What would you like to encode?\n";
-	cin >> EncodeString;
+	//cout << "What would you like to encode?\n";
+	//getline(cin, EncodeString);
 
-	encode(EncodeString);
-	decode();
+	//encode(EncodeString);
+	//decode();
 
-	outputFile.close();
+	string choice;
+
+
+	//encode a string of text
+	cout << "Would you like to encode some text? (y/n)\n";
+	getline(cin, choice);
+	if (choice.at(0) == 'y')
+	{
+		cout << "What would you like the file to be named? (include file extension)\n";
+		getline(cin, Filename);
+		outputFile.open(Filename, fstream::in | fstream::out | fstream::trunc);
+
+		cout << "What would you like to encode?\n";
+		getline(cin, EncodeString);//sends entire line to encode string, causes overflow
+		//Potential fix
+		//take the encoded string and split into a bunch of seperate strings and then send those individually into encode and put spaces between them all
+		//example string "Something cool to encode"
+
+		//-----------------TEST-----------------
+		
+		//find out where each of the spaces are
+		//create substrings between all of them
+		//if there is no right bound that is the last word in the string
+
+		//count how many spaces there are in the string
+		int count = 0;
+		int pos = 0;
+		bool reading = true;
+		while (true)
+		{
+			cout << "\nStarting Value is " << EncodeString << endl;
+			if (EncodeString.find(" ") == string::npos)break;//string::npos is the return if not found
+
+			//cout << EncodeString.find(" ", pos) << endl;
+
+			pos = EncodeString.find(" ", pos) + 1;
+			EncodeString = EncodeString.substr(pos+1);
+
+			cout << "\nEnding Value is " << EncodeString << endl;
+
+			count++;
+		}
+
+		cout << count << endl;
+
+		//-----------------TEST-----------------
+
+		encode(EncodeString);
+		
+		choice = "";
+		outputFile.close();
+	}
+
+	//Decoding a specific file
+	cout << "Would you like to decode a string from a file? (y/n)\n";
+	getline(cin, choice);
+	if(choice.at(0)=='y')
+	{
+		cout << "What file would you like to decode?\n";
+		getline(cin, Filename);
+
+		outputFile.open(Filename, fstream::in | fstream::out);//| fstream::trunc <- overwrites the file
+
+		decode();
+		outputFile.close();
+	}
+
+	//outputFile.close();
 	system("pause");
 }
 
@@ -63,20 +130,25 @@ void decode()
 {
 	//Might not have to close and reopen, check later
 	outputFile.close();
-	outputFile.open(Filename);
-
+	outputFile.open(Filename, fstream::in);
+	cout << "file is open\n";
 
 	//Use Dynamic Array
 	string values[100];
 
 	int n = 0;
-	while (!outputFile.eof())//Gets all the values from the file and puts them in the array
+
+	while ( outputFile >> values[n])//Gets all the values from the file and puts them in the array
 	{
-		outputFile >> values[n];
+		
+		//getline(outputFile, values[n]);
+		//cout << values[n] << " ";
+		//outputFile >> values[n];
 		n++;
 	}
 
-	for (int b = 0; b < n - 1; b++)
+
+	for (int b = 0; b < n; b++)
 	{
 		if (values[b].at(0) == 's')//checks if the value should be a string
 		{
@@ -103,6 +175,7 @@ void decodeInt(int value)
 	cout << value;
 }
 
+//Stack overflow called by massive recursion
 void encode(string value)
 {
 	std::this_thread::sleep_for(std::chrono::milliseconds(100));//makes me happy because it looks cool (remove for faster execution)
